0fcf392711885332e1b0feb0ad37f838340a9d5b
AmmarMagdy <ammar.m.badawi@gmail.com>
2020-07-02 18:25:33 +0200

    Add design home

---
+++ Sports/Controllers/HomeLeague/HomeLeagueViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HomeLeagueViewController.swift
+//  Sports
+//
+//  Created by Ammar.M on 7/2/20.
+//  Copyright © 2020 Ammar.M. All rights reserved.
+//
+
+import UIKit
+
+class HomeLeagueViewController: UIViewController {
+
+    @IBOutlet weak var leaguesTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
