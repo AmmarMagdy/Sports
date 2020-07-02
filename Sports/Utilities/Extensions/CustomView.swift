8973d8ef931ee4469cf024bd9c0282303b3a9437
AmmarMagdy <ammar.m.badawi@gmail.com>
2020-07-02 18:56:15 +0200

    create IBDesignable for custom view

---
+++ Sports/Utilities/Extensions/CustomView.swift
@@ -0,0 +1,88 @@
+//
+//  CustomView.swift
+//  Sports
+//
+//  Created by Ammar.M on 7/2/20.
+//  Copyright © 2020 Ammar.M. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class CustomView: UIView {
+    
+
+    @IBInspectable var cornerRadius: Float {
+        get {
+            return Float(layer.cornerRadius)
+        }
+        set {
+            layer.cornerRadius = CGFloat(newValue)
+        }
+    }
+    
+    
+    @IBInspectable var borderWidth: Double {
+        get {
+            return Double(self.layer.borderWidth)
+        }
+        set {
+            self.layer.borderWidth = CGFloat(newValue)
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            return UIColor(cgColor: self.layer.borderColor!)
+        }
+        set {
+            self.layer.borderColor = newValue?.cgColor
+        }
+    }
+    
+    @IBInspectable
+    var shadowPath: CGPath? {
+          get {
+              return layer.shadowPath
+          }
+          set {
+            self.layer.shadowPath = UIBezierPath(roundedRect: self.bounds, cornerRadius: self.layer.cornerRadius).cgPath
+          }
+    }
+    
+    @IBInspectable
+    var shadowOpacity: Float {
+        get {
+            return layer.shadowOpacity
+        }
+        set {
+            layer.shadowOpacity = newValue
+        }
+    }
+    
+    @IBInspectable
+    var shadowOffset: CGSize {
+        get {
+            return layer.shadowOffset
+        }
+        set {
+            layer.shadowOffset = newValue
+        }
+    }
+    
+    @IBInspectable var shadowColor: UIColor? {
+        get {
+            if let color = layer.shadowColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            if let color = newValue {
+                layer.shadowColor = color.cgColor
+            } else {
+                layer.shadowColor = nil
+            }
+        }
+    }
+}
+
