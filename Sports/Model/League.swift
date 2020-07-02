72e3437230e5d158aad1e51ddf2cc201e1e90cc5
AmmarMagdy <ammar.m.badawi@gmail.com>
2020-07-02 19:16:14 +0200

    league model

---
+++ Sports/Model/League.swift
@@ -0,0 +1,21 @@
+//
+//  League.swift
+//  Sports
+//
+//  Created by Ammar.M on 7/2/20.
+//  Copyright © 2020 Ammar.M. All rights reserved.
+//
+
+struct League : Codable {
+
+    let leagues : [Leagues]?
+}
+
+struct Leagues : Codable {
+
+    let idLeague : String?
+    let strLeague : String?
+    let strLeagueAlternate : String?
+    let strSport : String?
+}
+
