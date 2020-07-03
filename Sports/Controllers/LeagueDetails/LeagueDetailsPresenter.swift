4ddf74e1cbf86df5dd20ecc37dc8f98900af1bcf
AmmarMagdy <ammar.m.badawi@gmail.com>
2020-07-03 19:05:57 +0200

    design leagues Details

---
+++ Sports/Controllers/LeagueDetails/LeagueDetailsPresenter.swift
@@ -0,0 +1,9 @@
+//
+//  LeagueDetailsPresenter.swift
+//  Sports
+//
+//  Created by Ammar.M on 7/3/20.
+//  Copyright © 2020 Ammar.M. All rights reserved.
+//
+
+import Foundation
---
+++ Sports/Controllers/LeagueDetails/LeagueDetailsViewController.swift
@@ -0,0 +1,36 @@
+//
+//  LeagueDetailsViewController.swift
+//  Sports
+//
+//  Created by Ammar.M on 7/3/20.
+//  Copyright © 2020 Ammar.M. All rights reserved.
+//
+
+import UIKit
+
+class LeagueDetailsViewController: UIViewController {
+
+    @IBOutlet weak var leagueImageView: UIImageView!
+    @IBOutlet weak var nameLeagueLabel: UILabel!
+    @IBOutlet weak var sportsLeagueLabel: UILabel!
+    @IBOutlet weak var countryLeagueLabel: UILabel!
+    @IBOutlet weak var descLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
