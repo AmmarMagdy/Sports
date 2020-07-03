52070f849f9a214582123630c82b074429f574ce
AmmarMagdy <ammar.m.badawi@gmail.com>
2020-07-03 15:07:52 +0200

    enhancement home controller

--- Sports/Controllers/HomeLeague/HomeLeagueViewController.swift
+++ Sports/Controllers/HomeLeague/HomeLeagueViewController.swift
@@ -12,9 +12,39 @@ class HomeLeagueViewController: UIViewController {
 
     @IBOutlet weak var leaguesTableView: UITableView!
     
+    var presenter: HomeLeaguePresenter!
+    let leagueCell = "leagueCell"
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        presenter = HomeLeagueImplementation(view : self)
+        leaguesTableView.register(UINib(nibName: "HomeLeagueTableCell", bundle: nil), forCellReuseIdentifier: leagueCell)
+        presenter.fetchLeaguesFromAPI()
     }
 }
+
+// MARK: - UITableViewDataSource
+
+extension HomeLeagueViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return presenter.numberOfLeague
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "leagueCell") as! HomeLeagueTableCell
+        presenter.configure(cell: cell, forRow: indexPath.row)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        let cell = cell as! HomeLeagueTableCell
+        presenter.configure(cell: cell, forRow: indexPath.row)
+    }
+    
+}
+
+
